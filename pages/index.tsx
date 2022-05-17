4af58112625898b6c7b146c69bc3fbdaa1d512ce
krisograbek <krzysztof.ograbek@gmail.com>
2022-05-17 10:05:18 +0200

    read posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,9 @@ const Home = ({ posts }: Props) => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <>
+              <PostCard post={post} key={post.title} />
+            </>
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
